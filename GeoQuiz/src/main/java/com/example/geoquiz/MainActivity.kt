112fd766abf216c5e38c3eb21585b5d8fbbe7615
mojan <hojat72elect@gmail.com>
2022-05-24 18:49:48 -0400

    finished the 1st chapter

--- GeoQuiz/src/main/java/com/example/geoquiz/MainActivity.kt
+++ GeoQuiz/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -2,19 +2,26 @@ package com.example.geoquiz
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var trueButton:Button
-    private lateinit var falseButton:Button
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        trueButton=findViewById(R.id.button_true)
+        trueButton = findViewById(R.id.button_true)
         falseButton = findViewById(R.id.button_false)
 
+        trueButton.setOnClickListener {
+            Toast.makeText(this, getString(R.string.string_button_true), Toast.LENGTH_SHORT).show()
+        }
+        falseButton.setOnClickListener {
+            Toast.makeText(this, getString(R.string.String_button_false), Toast.LENGTH_SHORT).show()
+        }
     }
 }
