f0b44201a4ebf8c4eb30c73232f4eb5f30efa1db
mojan <hojat72elect@gmail.com>
2022-05-24 19:11:32 -0400

    partly customized the Toasts.

--- GeoQuiz/src/main/java/com/example/geoquiz/MainActivity.kt
+++ GeoQuiz/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.geoquiz
 
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -18,10 +19,25 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.button_false)
 
         trueButton.setOnClickListener {
-            Toast.makeText(this, getString(R.string.string_button_true), Toast.LENGTH_SHORT).show()
+            val toast1 =
+                Toast.makeText(
+                    this,
+                    getString(R.string.string_button_true),
+                    Toast.LENGTH_SHORT
+                )
+            toast1.setGravity(Gravity.TOP, 0, 0)
+            toast1.show()
         }
         falseButton.setOnClickListener {
-            Toast.makeText(this, getString(R.string.String_button_false), Toast.LENGTH_SHORT).show()
+            val toast2 =
+                Toast.makeText(
+                    this,
+                    getString(R.string.String_button_false),
+                    Toast.LENGTH_SHORT
+                )
+            toast2.setGravity(Gravity.CENTER, 0, 0)
+            toast2.show()
+
         }
     }
 }
