c91e78ca71cb5a8d93283e16fa76174b365abe0b
mojan <hojat72elect@gmail.com>
2022-05-24 17:46:35 -0400

    Initial commit

---
+++ GeoQuiz/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.geoquiz
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton:Button
+    private lateinit var falseButton:Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        trueButton=findViewById(R.id.button_true)
+        falseButton = findViewById(R.id.button_false)
+
+    }
+}
